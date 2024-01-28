6295694800edaeea646c38396ff4653cbe842399
hasnainzee <hasnain.zeenwala@gmail.com>
2024-01-28 14:13:41 +0530

    Display user prompt in a loop till the tracee is restarted

--- main.cpp
+++ main.cpp
@@ -3,93 +3,97 @@
 #include <unistd.h>
 #include <errno.h>
 #include <wait.h>
+#include <string.h>
 
-/* print a welcome message for entering HZDbg
- * fork a process and start tracing it and pause it as soon as it begins
- * print when it is paused
- * Resume it when user enters 'c'. It should print some message letting you know it has resumed.
- * After it is done free it from the Zombie state.
+#define TRACEE_RUNNING 0
+#define TRACEE_STOPPED 1
+typedef int tracee_state;
+
+/*
+ Launch debugger with "hzdbg /traceepath traceename"
 */
 
-void handle_user_action(char c, pid_t pid) {
+tracee_state handle_user_action(pid_t pid)
+{
+    char i;
+    printf(">>> ");
+    fflush(stdout);
+    i = getchar();
+    for (; getchar() != '\n' && getchar() != EOF;)
+        ;
     // continue program
-    if (c == 'c') {
+    if (i == 'c')
+    {
         // continue the process
         ptrace(PTRACE_CONT, pid, NULL, NULL);
+        return TRACEE_RUNNING;
     }
-
+    return TRACEE_STOPPED;
 }
 
-int main (int argc, char *argv[]) {
+void signal_receive_user_input_loop(pid_t pid, char *pname)
+{
+    for (;;)
+    {
+        int process_status;
+        if (waitpid(pid, &process_status, WUNTRACED | WCONTINUED) != -1)
+        {
+            if (WIFSTOPPED(process_status) != 0)
+            {
+                std::cout << pname << " is in a signal-delivery-stop state\n";
+                std::cout << pname << " received the " << strsignal(WSTOPSIG(process_status)) << " signal\n";
+
+                // Display debugger prompt till the tracee process has been resumed
+                while (handle_user_action(pid) == TRACEE_STOPPED);
+
+            }
+            else if (WIFEXITED(process_status) != 0)
+            {
+                std::cout << pname << " has exited with exit status: " << WEXITSTATUS(process_status) << "\n";
+                std::cout << "Tracee has exited, peace out!\n";
+                return;
+            }
+            else
+            {
+                std::cout << "Process hasn't stopped nor exited: [process status: " << process_status << "]\n";
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
     std::cout << "**************Hi! Welcome to HZDbg*******************\n";
-    if (argc > 1) {
+    if (argc > 1)
+    {
         /*
          * Fork the current process and call the "exec" syscall in child
          * to launch the actual program to be traced.
-        */
+         */
         int fork_result = fork();
-        if (fork_result == 0) {
+        if (fork_result == 0)
+        {
             // Child process
             // Child process asks parent to trace it.
-            if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1) { 
+            if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1)
+            {
                 std::cout << "Ptrace traceme was unsuccessful. Error code: " << errno << "\n";
                 return 1;
             }
             // Use exec system call to load the actual program that has to be traced.
-            // Since the process is being traced, after success of exec the process will be 
+            // Since the process is being traced, after success of exec the process will be
             // sent a SIGTRAP which will cause it to enter signal-delivery-stop.
-            int r = execl(argv[1], argv[2], (char *) NULL);
+            int r = execl(argv[1], argv[2], (char *)NULL);
             std::cout << "Exec result: " << r << std::endl;
         }
-        else {
+        else
+        {
             std::cout << "tracee process pid: " << fork_result << "\n";
-            // Wait on the child process to receive SIGTRAP
-            int process_status;
-            if (waitpid(fork_result, &process_status, WUNTRACED | WCONTINUED) != -1) {
-                // Check if the tracee received SIGTRAP
-                // SIGTRAP case requires special handling to distunguish it from
-                // syscall stops.
-                if (WIFSTOPPED(process_status) != 0) {
-                    std::cout << argv[2] << " is in a signal-delivery-stop state\n";
-                    if (WSTOPSIG(process_status) == SIGTRAP) {
-                        std::cout << "Process received SIGTRAP\n";
-                    }
-                    // send a PTRACE_GETSIGINFO query to confirm that the signal is SIGTRAP
-                    siginfo_t sig_info;
-                    ptrace(PTRACE_GETSIGINFO, fork_result, NULL, (void *)&sig_info);
-
-                    if (sig_info.si_code != SIGTRAP && sig_info.si_code != (SIGTRAP | 0x80)) {
-                        std::cout << "This wasn't a syscall stop. si_code: " << sig_info.si_code << "\n";
-                    }
-
-                    // Wait for user action
-                    char i;
-                    printf(">>> ");
-                    fflush(stdout);
-                    i = getchar();
-                    for(;getchar() != '\n' && getchar() != EOF;);
-                    handle_user_action(i, fork_result);
-
-                    // Wait to the process when it is done so it doesn't become a zombie.
-                    if (waitpid(fork_result, &process_status, WUNTRACED | WCONTINUED) != -1) {
-                        if (WIFEXITED(process_status) != 0) {
-                            std::cout << argv[1] << " has exitted\n";
-                        } else {
-                            std::cout << "process hasn't exitted\n";
-                        }
-
-                    } else {
-                        std::cout << "some problem with wait\n";
-                    }
-                }
-                else {
-                    std::cout << "Process isn't stopped, idk!\n";
-                }
-            }
-            
+            signal_receive_user_input_loop(fork_result, argv[2]);
         }
     }
-    else {
+    else
+    {
         std::cout << "No tracee file provided, exiting!\n";
     }
 
